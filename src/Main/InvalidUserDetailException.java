65a424ecfaf010441ffe3df3c40c10a772e4aa7b
angiratiwari <angiratiwari1102@gmail.com>
2024-12-19 16:08:39 +0530

    Added exception class

---
+++ src/Main/InvalidUserDetailException.java
@@ -0,0 +1,9 @@
+package Main;
+
+// InvalidUserDetailException.java
+public class InvalidUserDetailException extends Exception {
+    public InvalidUserDetailException(String message) {
+        super(message);
+    }
+}
+
