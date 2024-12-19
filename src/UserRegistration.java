074ef31c466994ee1a23e30aded4c92eb37d5174
angiratiwari <angiratiwari1102@gmail.com>
2024-12-19 10:29:59 +0530

    added UC5 to UC8

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -21,4 +21,15 @@ public class UserRegistration {
         return Pattern.matches("^[0-9]{2} [0-9]{10}$", mobileNumber);
     }
 
+    // UC5-UC8: Validate Password (all rules combined)
+    public boolean validatePassword(String password) {
+        String rule1 = "(?=.*[A-Z])"; // At least one uppercase
+        String rule2 = "(?=.*[0-9])"; // At least one digit
+        String rule3 = "(?=.*[@#$%^&-+=()])"; // At least one special character
+        String rule4 = "(?=.{8,})"; // Minimum 8 characters
+
+        String combinedRule = rule1 + rule2 + rule3 + rule4;
+        return Pattern.matches(combinedRule, password);
+    }
+
 }
